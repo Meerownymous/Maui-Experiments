20585d9faafc3694524a413e1fae22baca400b05
meerow <meerow@mirmx.de>
2023-11-28 21:03:24 +0100

    chore: smashed through the jungle of things to get this running in the ios simulator.

---
+++ MauiPlay/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiPlay;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ MauiPlay/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MauiPlay;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
